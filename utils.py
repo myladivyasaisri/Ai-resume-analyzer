3ed95b93024fff20635e025bc44963c47198074b
myladivyasaisri <divyasaisri429@gmail.com>
2026-06-25 17:04:34 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,7 @@
 import os
 import pdfplumber
 import docx
-import pytesseract
 from PIL import Image
-
-# OCR path (Windows)
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
 # ---------------- SKILLS ----------------
 SKILLS = [
